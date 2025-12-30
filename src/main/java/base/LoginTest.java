bb7e8e6474b63dc27ed8d734b539ffb9d268a93b
kedarlute <65060000+kedarlute@users.noreply.github.com>
2025-12-30 10:45:23 +0530

    Create LoginTest.java

---
+++ src/main/java/base/LoginTest.java
@@ -0,0 +1,57 @@
+package tests;
+
+/**
+ * @author: Kedarnath Lute
+ */
+
+import base.BaseTest;
+import com.aventstack.extentreports.Status;
+import constants.Constants;
+import listeners.RetryAnalyzer;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import com.aventstack.extentreports.MediaEntityBuilder;
+import utils.DriverManager;
+
+public class LoginTest extends BaseTest {
+
+   WebDriver driver;
+
+        @BeforeClass
+        public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        
+        }
+        
+        @Test(priority = 0)
+        public void loginTest() {
+        driver.findElement(By.id("username")).sendKeys(Constants.UserName);
+        driver.findElement(By.id("password")).sendKeys(Constants.Password);
+        driver.findElement(By.id("loginBtn")).click();
+
+        WebElement dashboard = driver.findElement(By.id("welcomeMsg"));
+        Assert.assertTrue(dashboard.isDisplayed(), "Login failed!");
+        }
+        
+        @Test(retryAnalyzer = RetryAnalyzer.class,dependsOnMethods = "loginTest"))
+        public void testOfficeDepotTitle() {
+            test = extent.createTest("VEYER by Office Depot Title Test");
+            //WebDriver driver = DriverManager.getDriver();
+            driver.get(Constants.BASE_URL);
+            String title = driver.getTitle();
+            test.info("Title is: " + title);
+            test.log(Status.INFO, "Launching browser");
+
+
+         Assert.assertEquals(driver.getTitle(),"VEYER by Office Depot - 3PL & eCommerce Fulfillment Services");
+            test.log(Status.PASS, "Title verified successfully");
+
+            test.pass("Step Passed",
+                    MediaEntityBuilder.createScreenCaptureFromPath("screenshot.png").build());
+
+
+         
+        }
+    }
