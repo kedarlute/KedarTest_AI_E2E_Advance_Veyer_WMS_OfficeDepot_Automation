51eb9c24dc63632d6c1be51fe7060a4c2d2cfcf7
kedarlute <65060000+kedarlute@users.noreply.github.com>
2025-12-30 12:47:46 +0530

    Create AILocator.java

---
+++ src/main/java/utils/AILocator.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import java.util.*;
+
+public class AILocator {
+
+    private final By primary;
+    private final List<By> alternatives;
+
+    public AILocator(By primary, List<By> alternatives) {
+        this.primary = primary;
+        this.alternatives = alternatives;
+    }
+
+    public By getPrimary() {
+        return primary;
+    }
+
+    public List<By> getAlternatives() {
+        return alternatives;
+    }
+}
